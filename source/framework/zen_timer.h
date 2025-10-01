bcf4b7f42b7e071b7c04b815629e8feff8269ac2
Nick Walton <contact@croakingkero.com>
2025-10-01 16:24:35 -0500

    Licensing

--- source/framework/zen_timer.h
+++ source/framework/zen_timer.h
@@ -1,3 +1,17 @@
+// Copyright [2025] [Nicholas Walton]
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 // You MUST call zen_Init() to use Zen Timer on Windows, otherwise the tick rate will be set at 1 and you'll get garbage.
