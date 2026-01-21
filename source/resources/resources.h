b79ed989ed42134921ef617b284f51c4d4b10576
Nick Walton <contact@croakingkero.com>
2026-01-21 15:39:16 -0600

    Latest resources

--- source/resources/resources.h
+++ source/resources/resources.h
@@ -4,12 +4,12 @@
 
 #include "framework.h"
 
-extern const font_t framework_font;
-extern const sprite_t framework_menu_cursor;
 extern const sprite_t framework_menu_folder_open;
 extern const sprite_t framework_menu_back;
-extern const sprite_t resources_gameplay_heli;
-extern const sprite_t resources_gameplay_coin;
+extern const font_t framework_font;
+extern const cursor_t framework_cursor;
 extern const sprite_t resources_gameplay_pipe_body;
+extern const sprite_t resources_gameplay_coin;
 extern const sprite_t resources_gameplay_pipe_top;
-extern const sound_music_t resources_music_ost1;
+extern const sprite_t resources_gameplay_heli;
+extern const sound_music_t resources_music_choppa;
